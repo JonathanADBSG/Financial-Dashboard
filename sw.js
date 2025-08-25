5cffe8b1cd904b99b82a0a691bfc5677f27c0f25
JonathanADBSG <jonathan.teixeira.salema@gmail.com>
2025-08-25 12:48:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'financial-report-v10';
+const CACHE_NAME = 'financial-report-v11';
 const APP_SHELL_URLS = [
   './',
   './index.html',
@@ -57,6 +57,7 @@ self.addEventListener('fetch', event => {
     );
   }
 });
+
 
 
 
